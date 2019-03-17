748532903f000593a55f51c5f9d3ea67ddbb6f1b
Rhys Xia <xrs4433@outlook.com>
2019-03-18 00:20:45 +0800

    developing

--- examples/babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    presets: ['@babel/preset-react', ['@babel/preset-env', {
-        modules: false
-    }]],
-    plugins: ["@babel/plugin-transform-runtime",]
-}
---
+++ examples/js/index.js
@@ -0,0 +1,7 @@
+import _classCallCheck from "@babel/runtime/helpers/classCallCheck";
+
+var A = function A() {
+  _classCallCheck(this, A);
+};
+
+export { A as default };

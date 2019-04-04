4d84f5c210f2a9052068491a983b6f841942043a
Rhys Xia <xrs4433@outlook.com>
2019-04-04 21:53:27 +0800

    :tada:Init project

--- examples/src/package/_test/index.tsx
+++
@@ -1,5 +0,0 @@
-describe('TEST', () => {
-    it('test1', () => {
-        expect(1).toBe(2)
-    })
-})
---
+++ test/tslint.test.ts
@@ -0,0 +1,5 @@
+describe('tslint', () => {
+  it('test', () => {
+    expect(1).toBe(1)
+  })
+})

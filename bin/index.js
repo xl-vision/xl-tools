5a496fb526f31695e87d976aeedebc6c0444211b
Rhys Xia <xrs4433@outlook.com>
2019-04-07 23:22:32 +0800

    :bug:fixed some bugs

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,33 @@
+#!/usr/bin/env node
+
 const program = require('commander')
-const chalk = require('chalk')
 const gulp = require('gulp')
 const pkg = require('../package.json')
 require('../lib/task')
 
 program
     .version(pkg.version)
-    .command('run [name]', 'run specified task')
-    .action(name => {
-        const task = gulp.task(name)
-        if(!task){
-            console.error(chalk.red(`The task '${name}' that you try to run is not found`))
-            process.exit(1)
+    .command('run <name>')
+    .action(function(name){
+        const metadata = { task: name }
+        // Gulp >= 4.0.0 (doesn't support events)
+        const taskInstance = gulp.task(name)
+        if (taskInstance === undefined) {
+            gulp.emit('task_not_found', metadata)
+            return
         }
-        task.apply(gulp)
+        const start = process.hrtime()
+        gulp.emit('task_start', metadata)
+        try {
+            taskInstance.apply(gulp)
+            metadata.hrDuration = process.hrtime(start)
+            gulp.emit('task_stop', metadata)
+            gulp.emit('stop')
+        } catch (err) {
+            err.hrDuration = process.hrtime(start)
+            err.task = metadata.task
+            gulp.emit('task_err', err)
+        }
     })
-    .parse(process.argv)
 
+program.parse(process.argv)

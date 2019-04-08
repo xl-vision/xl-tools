f03513ce696ab6402360be260c1929cf940dad54
Rhys Xia <xrs4433@outlook.com>
2019-04-08 15:18:18 +0800

    :construction: Working

--- bin/index.js
+++ bin/index.js
@@ -4,29 +4,25 @@ const program = require('commander')
 const gulp = require('gulp')
 const pkg = require('../package.json')
 require('../lib/task')
+const syncTask = require('./sync-task')
+
+syncTask(gulp)
 
 program
   .version(pkg.version)
   .command('run <name>')
   .action(function (name) {
-    const metadata = { task: name }
-    // Gulp >= 4.0.0 (doesn't support events)
-    const taskInstance = gulp.task(name)
-    if (taskInstance === undefined) {
-      gulp.emit('task_not_found', metadata)
-      return
-    }
-    const start = process.hrtime()
-    gulp.emit('task_start', metadata)
     try {
-      taskInstance.apply(gulp)
-      metadata.hrDuration = process.hrtime(start)
-      gulp.emit('task_stop', metadata)
-      gulp.emit('stop')
+      const task = gulp.series(name)
+      task(err => {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
+      })
     } catch (err) {
-      err.hrDuration = process.hrtime(start)
-      err.task = metadata.task
-      gulp.emit('task_err', err)
+      console.error(err)
+      process.exit(1)
     }
   })
 
---
+++ bin/sync-task.js
@@ -0,0 +1,42 @@
+const chalk = require('chalk')
+
+const tasks = {}
+
+function warn () {
+  var taskKeys = Object.keys(tasks)
+
+  if (!taskKeys.length) {
+    return
+  }
+
+  var taskNames = taskKeys.map(function (key) {
+    return tasks[key]
+  }).join(', ')
+
+  process.exitCode = 1
+
+  console.warn(
+    chalk.red('The following tasks did not complete:'),
+    chalk.cyan(taskNames)
+  )
+  console.warn(
+    chalk.red('Did you forget to signal async completion?')
+  )
+}
+
+function start (e) {
+  tasks[e.uid] = e.name
+}
+
+function clear (e) {
+  delete tasks[e.uid]
+}
+
+function logSyncTask (gulpInst) {
+  process.once('exit', warn)
+  gulpInst.on('start', start)
+  gulpInst.on('stop', clear)
+  gulpInst.on('error', clear)
+}
+
+module.exports = logSyncTask

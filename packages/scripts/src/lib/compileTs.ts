0133338a9da1f4f52c2a3beabcc16b2afa3974d1
Rhys Xia <xrs4433@outlook.com>
2020-05-17 15:28:19 +0800

    优化错误输出

--- packages/scripts/src/lib/compileTs.ts
+++ packages/scripts/src/lib/compileTs.ts
@@ -1,47 +1,47 @@
 import gulp from 'gulp'
 import gulpBabel from 'gulp-babel'
 import gulpTypescript from 'gulp-typescript'
-import streamToPromise from '../utils/stream2Promise'
 import getBabelConfig from './getBabelConfig'
+import chalk from 'chalk'
 
 export type Options = {
   isEs: boolean
   tsConfigFile: string
 }
 
-const reporter = gulpTypescript.reporter.defaultReporter()
-
 const overwriteConfig = {
   declaration: true,
   module: 'ESNext'
 }
 
-export default  (from: string | string[], to: string, options: Options) => {
+export default (from: string | string[], to: string, options: Options) => {
   const { isEs, tsConfigFile } = options
   const tsProject = gulpTypescript.createProject(tsConfigFile, overwriteConfig)
 
-  const tsResult = gulp.src(from).pipe(
-    tsProject({
-      error(err: gulpTypescript.reporter.TypeScriptError, ts: any) {
-        reporter.error!(err, ts)
-        process.exit(1)
-      },
-      finish: reporter.finish
-    })
-  )
+  return new Promise((resolve, reject) => {
 
-  const dts = tsResult.dts.pipe(gulp.dest(to))
-  const js = tsResult.js
-    .pipe(
-      gulpBabel({
-        ...getBabelConfig({
-          target: 'lib',
-          isTypescript: true,
-          isEs
-        })
+    const tsResult = gulp.src(from).pipe(
+      tsProject({
+        error(err) {
+          reject(err)
+        },
+        finish(_results){
+          resolve()
+        }
       })
     )
-    .pipe(gulp.dest(to))
 
-  return streamToPromise(dts, js)
+    tsResult.dts.pipe(gulp.dest(to))
+    tsResult.js
+      .pipe(
+        gulpBabel({
+          ...getBabelConfig({
+            target: 'lib',
+            isTypescript: true,
+            isEs
+          })
+        })
+      )
+      .pipe(gulp.dest(to))
+  })
 }

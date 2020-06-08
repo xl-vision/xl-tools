05428f92d601db188cea48f76cce35979010cf13
Rhys Xia <xrs4433@outlook.com>
2020-06-08 09:52:36 +0800

    修复在有虚拟网卡的电脑上无法显示正确的ip的问题

--- packages/scripts/src/utils/getPublicAddress.ts
+++ packages/scripts/src/utils/getPublicAddress.ts
@@ -2,15 +2,28 @@ import os from 'os'
 
 export default () => {
   const interfaces = os.networkInterfaces()
-  for(const name in interfaces) {
+  let virtualIp = ''
+  for (const name in interfaces) {
     const iface = interfaces[name]
-    if(!iface) {
+    if (!iface) {
       continue
     }
-    for(const info of iface) {
-      if(info.family === 'IPv4' && info.address !== '127.0.0.1' && !info.internal) {
+    for (const info of iface) {
+      if (
+        info.family === 'IPv4' &&
+        info.address !== '127.0.0.1' &&
+        !info.internal
+      ) {
+        // 当找不到其他ip时，使用虚拟机ip
+        if (name.startsWith('VMware') || name.startsWith('VirtualBox')) {
+          if (!virtualIp) {
+            virtualIp = info.address
+          }
+          continue
+        }
         return info.address
       }
     }
   }
+  return virtualIp
 }

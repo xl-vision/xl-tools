adba82044b0a1a4b3510ced19348807fcf9f41cb
Rhys Xia <xrs4433@outlook.com>
2020-06-09 15:02:14 +0800

    修改DemoBoxProps类型定义

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -4,14 +4,13 @@ import { MDXProvider } from '@mdx-js/react'
 import { Block as BaseBlock } from './mdLoader/codeBlock'
 
 export interface Block extends BaseBlock {
-  preview: React.ReactNode
+  preview: React.ReactFragment
 }
-
 
 export type DemoBoxProps = {
   children: React.ReactNode
-  desc: React.ReactNode
-  title: React.ReactNode
+  desc: React.ReactFragment
+  title: React.ReactFragment
   blocks: Array<Block>
 }
 
